85e85b3d80fbd593b5ff0120aa647ada1dac2338
Maxim <mistmax47@gmail.com>
2021-08-20 21:15:49 +0300

    кк

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,10 +9,15 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     Button yesBtn;
     Button noBtn;
+    Button showAnswer;
     TextView questionTextView;
+    int counter = 0;
+    Question[] answerUser =new Question[5];
     Question[] questions = {
             new Question(R.string.question1, true),
             new Question(R.string.question2, true),
@@ -29,30 +35,33 @@ public class MainActivity extends AppCompatActivity {
             questionIndex = savedInstanceState.getInt("questionIndex", 0);
         yesBtn = findViewById(R.id.yesBtn);
         noBtn = findViewById(R.id.noBtn);
+        showAnswer = findViewById(R.id.answer);
         questionTextView = findViewById(R.id.questionTextView);
         questionTextView.setText(questions[questionIndex].getQuestionText());
         yesBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (questions[questionIndex].isAnswerTrue())
-                    Toast.makeText(MainActivity.this,"Правильно", Toast.LENGTH_SHORT).show();
-                else
-                    Toast.makeText(MainActivity.this, "Не верный ответ", Toast.LENGTH_SHORT).show();
-                questionIndex++;
-                questionTextView.setText(questions[questionIndex].getQuestionText());
+                checkAnswer(true);
             }
         });
         noBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!questions[questionIndex].isAnswerTrue())
-                    Toast.makeText(MainActivity.this,"Правильно", Toast.LENGTH_SHORT).show();
-                else
-                    Toast.makeText(MainActivity.this, "Не верный ответ", Toast.LENGTH_SHORT).show();
-                questionIndex++;
-                questionTextView.setText(questions[questionIndex].getQuestionText());
+                checkAnswer(false);
             }
         });
+        showAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AnswerActivity.class);
+                intent.putExtra("answer", questions[questionIndex].isAnswerTrue());
+                intent.putExtra("count", counter);
+                startActivity(intent);
+            }
+        });
+    }
+    private String attachAnswerQuestion(String question, boolean answer) {
+        return question + " - " + "ваш ответ: " + (answer ? getString(R.string.yes) : getString(R.string.no));
     }
 
     @Override
@@ -60,6 +69,15 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(savedInstanceState);
         savedInstanceState.putInt("questionIndex", questionIndex);
 
+    }
+    public void checkAnswer(boolean btn) {
+        if (questions[questionIndex].isAnswerTrue() && btn || (!questions[questionIndex].isAnswerTrue() && !btn)) {
+            Toast.makeText(MainActivity.this, "Правильно", Toast.LENGTH_SHORT).show();
+            counter++;
+        }else
+            Toast.makeText(MainActivity.this, "Не верный ответ", Toast.LENGTH_SHORT).show();
+        questionIndex = (questionIndex+1)%questions.length;
+        questionTextView.setText(questions[questionIndex].getQuestionText());
     }
     @Override
     public void onStart() {
