0a5e72dae8bb385484db7e4d964ee383c3e0ea47
Maxim <mistmax47@gmail.com>
2021-09-01 14:44:00 +0300

    Звершение вопросника

--- app/src/main/java/com/example/myapplication/ResultActivity.java
+++ app/src/main/java/com/example/myapplication/ResultActivity.java
@@ -6,6 +6,7 @@ import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
     TextView q_AndAnswers;
+    TextView resultScores;
     String[] resultArr;
     int result;
 
@@ -14,9 +15,15 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
         q_AndAnswers = findViewById(R.id.q_and_answers);
-        resultArr = getIntent().getStringArrayExtra("qAndAnswer");
+        resultScores = findViewById(R.id.youScores);
+        resultArr = getIntent().getStringArrayExtra("qAndAnswers");
         for (int i = 0; i < resultArr.length; i++) {
             q_AndAnswers.append(resultArr[i] + "\n");
         }
+        Bundle extras = getIntent().getExtras();
+            if (extras != null) {
+                result = extras.getInt("counter");
+            }
+        resultScores.append(result+"");
     }
 }
